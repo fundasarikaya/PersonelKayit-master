71054e502a881926e63c94cf8b609a8c7ea92e95
fundasarikaya <fundasarikaya58@gmail.com>
2020-07-25 00:52:48 +0300

    npm install -save lodash

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import {View, Text, ListView} from 'react-native';
 import {connect} from 'react-redux';
@@ -6,6 +7,14 @@ import {employeesListData} from '../actions';
 class EmployeeList extends Component {
   componentWillMount() {
     this.props.employeesListData();
+  }
+
+  //propertyler componente ulaştığı zaman componentWillReciveProps
+  componentWillReceiveProps(nextProps) {
+    const datasource = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2,
+    });
+    this.datasource = datasource.cloneWithRows(nextProps.employeesArray);
   }
   render() {
     return (
@@ -25,3 +34,6 @@ export default connect(
   mapStateToProps,
   {employeesListData},
 )(EmployeeList);
+
+
+//npm install -save lodash ile gelen objeleri array icerisine id ile eklememizi saglar
